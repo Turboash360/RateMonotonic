884de0152f4fa9a175f01638150c79c0516fe582
Emmanuel <programmingeman@gmail.com>
2017-05-01 15:22:38 -0700

    Remove not needed comments.

--- ratemonotonic.c
+++ ratemonotonic.c
@@ -1,25 +1,7 @@
 /*
 * Emmanuel A. Castillo, 004268444
-* ecasti107@cougars.csusm.edu
-* CS 351 Programming Languages
-* 11/25/2016
-* C1A4E1_main.c
-* Win7
-* Visual C++ 11.0
 *
 * Description:
-* The below code uses the natoenglishtranslate.h
-* to read in a words.txt file and display its
-* corresponding translation. In addition,
-* this file contains the implementations of the nato to english or vice versa
-* translation. In addition, inline functions are used for the translation assistance.
-* The following functions exist:
-* 1) Shut program if error exist.
-* 2) Open a file for reading.
-* 3) Compare string insenstive to case.
-* 4) Check what type of translation to occur based on the first 8 chars or lower of the parsed file.
-* 5) Translate letters to NATO code.
-* 6) Translate NATO code to letters.
 */
 #include <ctype.h>
 
